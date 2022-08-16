85b03750c089f2946ecdb51e48f4a83d3ea88893
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-08-16 15:34:17 +0200

    adding update procedure

--- BusinessRules_General.sql
+++ BusinessRules_General.sql
@@ -297,3 +297,111 @@ EXEC dbo.sp_Create_ScriptObjects
         ,@ScriptObject = 0;
 
 
+
+
+
+
+
+
+ALTER PROCEDURE dbo.sp_Update_Parameters
+	(
+	 @Query_ID INT
+	,@Query_key VARCHAR(20)
+	,@new_query_value NVARCHAR(MAX) = NULL
+	,@new_query_parameter_Description VARCHAR(500) = NULL
+	,@new_query_table_related VARCHAR(500) = NULL
+	,@is_enabled TINYINT = 1
+	)
+/*
+Usage:
+
+	EXEC dbo.sp_Update_Parameters 
+			@query_id = 10203
+			,@Query_key = '$wherekey2'
+			,@new_query_value = ' '
+			,@new_query_parameter_Description = '
+			,@new_query_table_related = '
+			,@is_enabled = 1
+
+
+Change Log:
+*/
+
+AS
+BEGIN
+	
+	--DECLARE @query_ID INT = 10203
+	--DECLARE @Query_key VARCHAR(20) = '$wherekey2'
+
+
+	IF OBJECT_ID('tempdb..#temp123','U') IS NOT NULL
+	DROP TABLE #temp123
+
+	SELECT  
+		 id
+		,parameter_version
+		,parameter_Active
+		,query_id
+		,[query_key]
+		,[query_value]
+
+		INTO #temp123
+	
+	FROM dbo.BusinessRules_Parameters
+	WHERE 
+		[query_id] = @query_id
+	AND query_key =  @Query_key
+	ORDER BY ID ASC
+
+
+SELECT * FROM #temp123
+DECLARE @pam_ver INT = (SELECT max(parameter_version) from BusinessRules_Parameters where [query_id] = @query_id AND query_key =  @Query_key)
+declare @pam_row_id INT = (select max(id) from  BusinessRules_Parameters where [query_id] = @query_id AND query_key =  @Query_key)
+
+
+-- only to disable parameter!
+
+-- Update procedure and replace the parameter with "1=1" or smth.
+
+IF (@is_enabled = 0)
+BEGIN
+	UPDATE dbo.BusinessRules_Parameters 
+	SET parameter_active = 0
+	WHERE
+		query_id = @query_id
+	and query_key = @Query_key
+	and id = @pam_row_id
+END
+
+
+-->>>>>>>>>>>>>>> BEGIN TRAN
+
+-- INSERT NEW VaLUE
+IF (@is_enabled = 1)
+BEGIN
+	 INSERT INTO dbo.BusinessRules_Parameters ([query_id], [query_parameter_Description], [query_parameter_tableRelated], [query_key], [query_value], parameter_version, parameter_active)
+	 SELECT 
+		 @query_id 
+		,@new_query_parameter_Description
+		,@new_query_table_related
+		,@Query_key 
+		,@new_query_value 
+		,@pam_ver + 1
+		,@is_enabled 
+
+	-- UPDATE OLD VALUEs
+
+END
+
+--Run CREATE procedure
+
+
+EXEC dbo.sp_Create_Objects 
+			@query_id = @query_id
+			,@persistObject = 2
+
+
+---- >>>>>>>>>>>>>>> COMMIT TRAN	
+	
+END;
+GO
