ffb5e0b0a94689e91fa6d61a773180401e4fd83f
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-08-16 15:35:06 +0200

    adding log table

--- BusinessRules_General.sql
+++ BusinessRules_General.sql
@@ -104,6 +104,22 @@ CREATE TABLE dbo.BusinessRules_Parameters
 
 
 
+ IF object_id ('BusinessRules_Executions', 'U') IS NOT  NULL
+DROP TABLE dbo.BusinessRules_Executions
+
+CREATE TABLE dbo.BusinessRules_Executions
+(
+ id INT IDENTITY(1,1) NOT NULL
+,query_id INT NOT NULL  
+,query_execution TINYINT NOT NULL DEFAULT(0) -- 0 - rule is on; 1 - rule is off
+,store_version TINYINT NOT NULL DEFAULT(0) -- 0 - storing version is on; 1 - storing version if off
+,user_created VARCHAR(50) NOT NULL DEFAULT (suser_name())
+,date_created DATETIME NOT NULL DEFAULT (GETDATE())
+,CONSTRAINT PK_BussinesRulesExecution_QueryID PRIMARY KEY CLUSTERED (query_id)
+);
+
+
+
 /*******************
 Populate table
 *******************/
