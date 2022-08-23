4f4988251a06922e29d682bab6dea810ac2fbff5
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-08-23 07:38:59 +0200

    code cleaning

--- BusinessRules_General.sql
+++ BusinessRules_General.sql
@@ -414,16 +414,6 @@ GO
 -- Scheduling and running
 --------------------------
 
-CREATE TABLE dbo.BusinessRules_Executions
-(
- id INT IDENTITY(1,1) NOT NULL
-,query_id INT NOT NULL  
-,query_execution TINYINT NOT NULL DEFAULT(0) -- 0 - rule is on; 1 - rule is off
-,user_created VARCHAR(50) NOT NULL DEFAULT (suser_name())
-,date_created DATETIME NOT NULL DEFAULT (GETDATE())
-,CONSTRAINT PK_BussinesRulesExecution_QueryID PRIMARY KEY CLUSTERED (query_id)
-);
-
 
 INSERT INTO dbo.BusinessRules_Executions (query_id, Query_execution)
 SELECT 
